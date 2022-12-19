c69c246488a418d932d866b0d4867fdd2fb1e7e8
pudongping <276558492@qq.com>
2022-12-19 21:30:18 +0800

    auto generate menu

---
+++ daily_deploy_menu.sh
@@ -0,0 +1,34 @@
+#/bin/bash
+
+function console_blue() {
+    echo -e "\033[36m[ $1 ]\033[0m"
+}
+
+function console_green() {
+    echo -e "\033[32m[ $1 ]\033[0m"
+}
+
+function console_orangered() {
+    echo -e "\033[31m\033[01m[ $1 ]\033[0m"
+}
+
+function console_yellow() {
+    echo -e "\033[33m\033[01m[ $1 ]\033[0m"
+}
+
+function auto_generate_menu() {
+  git pull origin main
+  ./script/mac-spider-blog-title
+  rm -rf source/menu/index.md
+  mv script/blog-menu.md source/menu/index.md
+  nvm use 16.2.0
+  hexo cl
+  hexo g
+  hexo d
+  git status
+  git add -A
+  git commit -m "auto generate menu"
+  git push -u origin main
+}
+
+auto_generate_menu
