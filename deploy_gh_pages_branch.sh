4c1af6784c68c58c4a849b3aff6ba0bea2c77603
pudongping <276558492@qq.com>
2023-10-18 02:05:41 +0800

    重新整理下

--- deploy_gh_pages_branch.sh
+++ deploy_gh_pages_branch.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# deploy gh-pages branch
 
 base_path=$(cd `dirname $0`; pwd)
 
@@ -20,19 +21,11 @@ function console_yellow() {
     echo -e "\033[33m\033[01m[ $1 ]\033[0m"
 }
 
-function auto_generate_menu() {
-  git pull origin main
-  ./script/spider-blog-title
-  rm -rf source/menu/index.md
-  mv ./blog-menu.md source/menu/index.md
+function deploy_2_gh_pages() {
   nvm use 16.2.0
   hexo cl
   hexo g
   hexo d
-  git status
-  git add -A
-  git commit -m "auto generate menu"-`date +"%Y-%m-%d_%H:%M:%S"`
-  git push -u origin main
 }
 
-auto_generate_menu
+deploy_2_gh_pages
